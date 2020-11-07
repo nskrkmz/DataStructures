7eeda742d35b7acb3615da58a2bce67c9ae3a18b
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-11-07 23:15:08 +0300

    Create LL..

---
+++ LinkedList.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+//include<string.h>
+
+struct musteri {
+	char * isim ;
+	int tutar ;
+};
+
+typedef struct musteri Musteri ;
+
+//Fonksiyonlarýn deklerasyonlari
+void ekle();
+void yazdirAd();
+void yazdirTutar();
+
+int main (void){
+	
+	
+}// main() func. sonu
+
+void ekle(){
+	
+
+}// ekle() func. sonu
+void yazdirAd(){
+	
+	
+}//yazdirAd() func. sonu
+
+void yazdirTutar(){
+	
+	
+}//yazdirTutar() func. sonu 
+
+
+
