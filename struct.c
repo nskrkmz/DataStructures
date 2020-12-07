4ea57bc29bd30abacf8c1ab66a1878460702853a
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-12-07 17:21:48 +0300

    the project started

---
+++ struct.c
@@ -0,0 +1,51 @@
+/*
+*	Abdullah Enes Korkmaz
+*	18061263
+*	enes.korkmaz@bil.omu.edu.tr	
+*	s->06.12.20
+*	f->xx.12.20
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+// ürün adý max 2*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
